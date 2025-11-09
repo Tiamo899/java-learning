d7cf12a9d3cccbd5f7dd1587352a3027797962ab
Tiamo899 <3502216024@qq.com>
2025-11-09 17:16:03 +0800

    加入Calculator方法

---
+++ src/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("除数不能为0");
+        return (double) a / b;
+    }
+}
--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -1,5 +1,10 @@
 public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("Hello GitHub!");
+        Calculator calc = new Calculator();
+        System.out.println("加法: " + calc.add(5, 3));
+        System.out.println("减法: " + calc.subtract(5, 3));
+        System.out.println("乘法: " + calc.multiply(5, 3));
+        System.out.println("除法: " + calc.divide(5, 3));
     }
 }
