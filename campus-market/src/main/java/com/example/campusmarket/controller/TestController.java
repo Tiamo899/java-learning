fa8663c81b3e72ed588756de3fbdc196b0859491
Tiamo899 <3502216024@qq.com>
2025-11-19 19:43:41 +0800

    新建了一个spring boot项目校园二手交易平台

---
+++ campus-market/src/main/java/com/example/campusmarket/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.campusmarket.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "校园二手交易平台后端启动成功！Tiamo899 ";
+    }
+}
